22bf17c31eca4d0771ebfdfd8f4b41c8e01c67e7
AidanRouai <aidanrouai@gmail.com>
2025-04-25 20:26:56 +0100

    Ready for submission

--- src/Components/ProductList.tsx
+++ src/Components/ProductList.tsx
@@ -21,12 +21,13 @@ export const ProductList = (props: ContentAreaProps) => {
 					<div key={item.name} className="product">
 						<div className="product-top-bar">
 							<h2>{item.name}</h2>
-							<p> £{item.price.toFixed(2)} ({item.rating}/5)</p>
+							<p> (£{item.price.toFixed(2)}) ({item.rating}/5)</p>
 						</div>
 						<img src={"./src/Assets/Product_Images/" + item.image_link}></img>
 						<button
 							onClick={() => props.addToBasket(item)}
 							disabled={item.quantity === 0}
+							value={item.id} 
 						>
 							{item.quantity === 0 ? 'Out of stock' : 'Add to basket'}
 						</button>
